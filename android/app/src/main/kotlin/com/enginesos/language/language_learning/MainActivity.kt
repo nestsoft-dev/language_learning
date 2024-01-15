77535f032897ee91a983bd69a7f79d1db65c62fe
Obetta ikenna <obettaikenna19@gmail.com>
2024-01-15 12:05:57 +0100

    Starting

---
+++ android/app/src/main/kotlin/com/enginesos/language/language_learning/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.enginesos.language.language_learning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:language_learning/presentation/pages/splash.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Language Learning',
+      theme: ThemeData(
+    
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
+
+/*
+echo "# language_learning" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nestsoft-dev/language_learning.git
+git push -u origin main*/
